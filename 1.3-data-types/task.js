05ff8c6422c1d471a6715b62233efdc9fee8bcba
Katya Nikolaeva <nikolaus12@yandex.ru>
2020-06-14 18:49:49 +0300

    Third task completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,46 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+  if (typeof percent === 'string') {
+    percent = Number(percent);
+  }
+
+  if (typeof percent !== 'number' || Number.isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  }
+
+  if (typeof contribution === 'string') {
+    contribution = Number(contribution);
+  }
+
+  if (typeof contribution !== 'number' || Number.isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  }
+
+  if (typeof amount === 'string') {
+    amount = Number(amount);
+  }
+
+  if (typeof amount !== 'number' || Number.isNaN(amount)) {
+    return `Параметр "Общая сумма" содержит неправильное значение ${amount}`;
+  }
+
+  let principal = amount - contribution;
+  let months = (date.getFullYear() - new Date().getFullYear()) * 12;
+  let P = percent / 100 / 12;
+  let monthlyPayment = principal * (P + P / (((1 + P) ** months) - 1));
+
+  let totalAmount = monthlyPayment * months;
+
+  return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+  if (Boolean(name) === false) {
+    name = 'Аноним';
+  }
+
+  return `Привет, мир! Меня зовут ${name}`;
 }
